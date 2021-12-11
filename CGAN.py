520aff93683646b374695eb1c7f1b8f274de8585
Sang3028 <Sang3028@gmail.com>
2021-12-11 09:50:50 +0900

    featurn mathcing added

--- CGAN.py
+++ CGAN.py
@@ -18,36 +18,31 @@ import torch
 
 os.makedirs("images", exist_ok=True)
 
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-    parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-    parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-    parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-    parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-    parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
-    parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-    parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-    parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
-    parser.add_argument("--trial", type=int, default=1, help="-th trial")
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
+parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+opt = parser.parse_args()
+print(opt)
 
-    opt = parser.parse_args()
+img_shape = (opt.channels, opt.img_size, opt.img_size)
 
-    opt.img_shape = (opt.channels, opt.img_size, opt.img_size)
-
-    print(opt)
-
-    return opt
+cuda = True if torch.cuda.is_available() else False
 
 
 class Generator(nn.Module):
-    def __init__(self, opt):
+    def __init__(self):
         super(Generator, self).__init__()
 
         self.label_emb = nn.Embedding(opt.n_classes, opt.n_classes)
-        self.img_shape = opt.img_shape
 
         def block(in_feat, out_feat, normalize=True):
             layers = [nn.Linear(in_feat, out_feat)]
@@ -61,7 +56,7 @@ class Generator(nn.Module):
             *block(128, 256),
             *block(256, 512),
             *block(512, 1024),
-            nn.Linear(1024, int(np.prod(opt.img_shape))),
+            nn.Linear(1024, int(np.prod(img_shape))),
             nn.Tanh()
         )
 
@@ -69,18 +64,18 @@ class Generator(nn.Module):
         # Concatenate label embedding and image to produce input
         gen_input = torch.cat((self.label_emb(labels), noise), -1)
         img = self.model(gen_input)
-        img = img.view(img.size(0), * self.img_shape)
+        img = img.view(img.size(0), *img_shape)
         return img
 
 
 class Discriminator(nn.Module):
-    def __init__(self, opt):
+    def __init__(self):
         super(Discriminator, self).__init__()
 
         self.label_embedding = nn.Embedding(opt.n_classes, opt.n_classes)
 
         self.model = nn.Sequential(
-            nn.Linear(opt.n_classes + int(np.prod(opt.img_shape)), 512),
+            nn.Linear(opt.n_classes + int(np.prod(img_shape)), 512),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(512, 512),
             nn.Dropout(0.4),
@@ -98,129 +93,118 @@ class Discriminator(nn.Module):
         return validity
 
 
+# Loss functions
+adversarial_loss = torch.nn.MSELoss()
 
-if __name__ == "__main__":
-    opt = parse_opt()
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
 
-    cuda = True if torch.cuda.is_available() else False
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    adversarial_loss.cuda()
 
-    # Loss functions
-    adversarial_loss = torch.nn.MSELoss()
-
-    # Initialize generator and discriminator
-    generator = Generator(opt)
-    discriminator = Discriminator(opt)
-
-    if cuda:
-        generator.cuda()
-        discriminator.cuda()
-        adversarial_loss.cuda()
-
-    # Configure data loader
-    os.makedirs("./data/mnist", exist_ok=True)
-    dataloader = torch.utils.data.DataLoader(
-        datasets.MNIST(
-            "./data/mnist",
-            train=True,
-            download=True,
-            transform=transforms.Compose(
-                [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-            ),
+# Configure data loader
+os.makedirs("./data/mnist", exist_ok=True)
+dataloader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "./data/mnist",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
         ),
-        batch_size=opt.batch_size,
-        shuffle=True,
-    )
+    ),
+    batch_size=opt.batch_size,
+    shuffle=True,
+)
 
-    # Optimizers
-    optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
 
-    FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
-    LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
-
-
-    def sample_image(n_row, batches_done):
-        """Saves a grid of generated digits ranging from 0 to n_classes"""
-        # Sample noise
-        z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
-        # Get labels ranging from 0 to n_classes for n rows
-        labels = np.array([num for _ in range(n_row) for num in range(n_row)])
-        labels = Variable(LongTensor(labels))
-        gen_imgs = generator(z, labels)
-        save_image(gen_imgs.data, f"images/CGAN_{opt.trial}_{batches_done}.png", nrow=n_row, normalize=True)
+FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
 
-    # ----------
-    #  Training
-    # ----------
+def sample_image(n_row, batches_done):
+    """Saves a grid of generated digits ranging from 0 to n_classes"""
+    # Sample noise
+    z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
+    # Get labels ranging from 0 to n_classes for n rows
+    labels = np.array([num for _ in range(n_row) for num in range(n_row)])
+    labels = Variable(LongTensor(labels))
+    gen_imgs = generator(z, labels)
+    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
 
-    for epoch in range(opt.n_epochs):
-        for i, (imgs, labels) in enumerate(dataloader):
 
-            batch_size = imgs.shape[0]
+# ----------
+#  Training
+# ----------
+feature_matching = False
 
-            # Adversarial ground truths
-            valid = Variable(FloatTensor(batch_size, 1).fill_(1.0), requires_grad=False)
-            fake = Variable(FloatTensor(batch_size, 1).fill_(0.0), requires_grad=False)
+for epoch in range(opt.n_epochs):
+    for i, (imgs, labels) in enumerate(dataloader):
 
-            # Configure input
-            real_imgs = Variable(imgs.type(FloatTensor))
-            labels = Variable(labels.type(LongTensor))
+        batch_size = imgs.shape[0]
 
-            # -----------------
-            #  Train Generator
-            # -----------------
+        # Adversarial ground truths
+        valid = Variable(FloatTensor(batch_size, 1).fill_(1.0), requires_grad=False)
+        fake = Variable(FloatTensor(batch_size, 1).fill_(0.0), requires_grad=False)
 
-            optimizer_G.zero_grad()
+        # Configure input
+        real_imgs = Variable(imgs.type(FloatTensor))
+        labels = Variable(labels.type(LongTensor))
 
-            # Sample noise and labels as generator input
-            z = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
-            gen_labels = Variable(LongTensor(np.random.randint(0, opt.n_classes, batch_size)))
+        # -----------------
+        #  Train Generator
+        # -----------------
 
-            # Generate a batch of images
-            gen_imgs = generator(z, gen_labels)
+        optimizer_G.zero_grad()
 
-            # Loss measures generator's ability to fool the discriminator
-            validity = discriminator(gen_imgs, gen_labels)
-            g_loss = adversarial_loss(validity, valid)
+        # Sample noise and labels as generator input
+        z = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
+        gen_labels = Variable(LongTensor(np.random.randint(0, opt.n_classes, batch_size)))
 
-            g_loss.backward()
-            optimizer_G.step()
+        # Generate a batch of images
+        gen_imgs = generator(z, gen_labels)
 
-            # ---------------------
-            #  Train Discriminator
-            # ---------------------
+        # Loss measures generator's ability to fool the discriminator
+        validity = discriminator(gen_imgs, gen_labels)
+        g_loss = adversarial_loss(validity, valid)
+        if feature_matching:
+            g_loss_feature = nn.MSELoss(discriminator(gen_imgs, gen_labels), discriminator(real_imgs, labels))
+            g_loss = g_loss + g_loss_feature
 
-            optimizer_D.zero_grad()
+        g_loss.backward()
+        optimizer_G.step()
 
-            # Loss for real images
-            validity_real = discriminator(real_imgs, labels)
-            d_real_loss = adversarial_loss(validity_real, valid)
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
 
-            # Loss for fake images
-            validity_fake = discriminator(gen_imgs.detach(), gen_labels)
-            d_fake_loss = adversarial_loss(validity_fake, fake)
+        optimizer_D.zero_grad()
 
-            # Total discriminator loss
-            d_loss = (d_real_loss + d_fake_loss) / 2
+        # Loss for real images
+        validity_real = discriminator(real_imgs, labels)
+        d_real_loss = adversarial_loss(validity_real, valid)
 
-            d_loss.backward()
-            optimizer_D.step()
+        # Loss for fake images
+        validity_fake = discriminator(gen_imgs.detach(), gen_labels)
+        d_fake_loss = adversarial_loss(validity_fake, fake)
 
-            print(
-                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-                % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
-            )
+        # Total discriminator loss
+        d_loss = (d_real_loss + d_fake_loss) / 2
 
-            batches_done = epoch * len(dataloader) + i
-            if batches_done % opt.sample_interval == 0:
-                sample_image(n_row=10, batches_done=batches_done)
+        d_loss.backward()
+        optimizer_D.step()
 
-    # Save model
-    os.makedirs("./data/ckpt", exist_ok=True)
-    ckpt_path = os.path.join("./data/ckpt", f"CGAN_trial{opt.trial}.pth")
-    torch.save({
-        "generator": generator.state_dict(),
-        "discriminator": discriminator.state_dict()
-    }, ckpt_path)
-    print("Saved checkpoint:", ckpt_path)
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+        )
+
+        batches_done = epoch * len(dataloader) + i
+        if batches_done % opt.sample_interval == 0:
+            sample_image(n_row=10, batches_done=batches_done)
